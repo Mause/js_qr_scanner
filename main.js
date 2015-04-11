38633d23a38483454f6982677db162281ed86bf6
Dominic May <jack.thatch@gmail.com>
2015-04-11 14:20:44 +0800

    Stupid api integration

--- main.js
+++ main.js
@@ -3,17 +3,78 @@ navigator.getUserMedia = navigator.getUserMedia ||
                          navigator.mozGetUserMedia ||
                          navigator.msGetUserMedia;
 
-function alert(str) {
-    document.getElementById("alert").innerHTML += (
-        "" + str + "<br/>"
-    )
+// function alert(str) {
+//     document.getElementById("alert").innerHTML += (
+//         "" + str + "<br/>"
+//     )
+// }
+
+function QrAPI(apiUrl, apiPassword) { this.apiUrl = apiUrl; }
+QrAPI.prototype = {
+    getHistory: function getHistory() {
+        return $.getJSON(
+            this.apiUrl,
+            {
+                qrcode: "",
+                password: this.apiPassword
+            }
+        );
+    },
+
+    scanRequest: function scanRequest(barcode) {
+        'use strict';
+        return $.post(
+            this.apiUrl,
+            {
+                qrcode: barcode,
+                password: this.apiPassword
+            },
+            function success(data) {
+                if (data.status < 1) {
+                    var error;
+
+                    if (data.error.length <= 0) {
+                        error = "Unknown error occured";
+                    } else {
+                        error = data.error;
+                    }
+                    alert(error);
+
+                } else if (data.paid <= 0){
+                    alert("Hasn't paid!");
+
+                } else {
+                    var user = data.user.user,
+                        firstName = user.first_name,
+                        lastName = user.last_name,
+                        successMessage = "";
+
+                    if (firstName.length > 0) {
+                        successMessage = firstName;
+                        if (lastName.length > 0) {
+                            successMessage += lastName;
+                        }
+                    } else {
+                        successMessage = "Sign-in successful";
+                    }
+
+                    alert(successMessage);
+                }
+            },
+            'json'
+        )
+    }
 }
 
 function QRScanner() {}
 QRScanner.prototype = {
     gum_success: function gum_success(stream) {
-        alert("Success!");
+        // alert("Success!");
         this.video.src = window.URL.createObjectURL(stream)
+        this.api = new QrAPI(
+            "http://requestb.in/14zv6j91",
+            "password"
+        );
     },
 
     gum_failure: function gum_failure(error) {
@@ -28,7 +89,7 @@ QRScanner.prototype = {
         this.ctx.drawImage(this.video, 0, 0)
 
         this.do_decode();
-        setTimeout(this.timerCallback.bind(this), 1500);
+        setTimeout(this.timerCallback.bind(this), 125);
     },
 
     do_decode: function do_decode() {
@@ -41,7 +102,7 @@ QRScanner.prototype = {
 
     qrcode_callback: function qrcode_callback(data) {
         document.getElementById("data").innerHTML = data;
-        // console.log(data);
+        this.api.scanRequest(data);
     },
 
     play_callback: function play_callback() {
@@ -61,15 +122,16 @@ QRScanner.prototype = {
         this.video = document.getElementById("video");
         this.c1 = document.getElementById("qr-canvas");
         this.ctx = this.c1.getContext("2d");
+        this.api = null;
 
         this.video.addEventListener("play", this.play_callback.bind(this), false);
 
-        qrcode.callback = this.qrcode_callback;
+        qrcode.callback = $.proxy(this.qrcode_callback, this);
         qrcode.debug = true;
 
         navigator.getUserMedia(
             {video: {}},
-            this.gum_success,
+            $.proxy(this.gum_success, this),
             this.gum_failure
         )
     }
@@ -78,7 +140,7 @@ QRScanner.prototype = {
 
 $(document).ready(function(){
 
-    alert(qrcode);
+    // alert(qrcode);
 
     try {
         new QRScanner().ready()

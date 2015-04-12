252a416d3c6d4da3d76234086dbb3365804eca0e
Dominic May <jack.thatch@gmail.com>
2015-04-12 19:41:08 +0800

    Split out callback

--- main.js
+++ main.js
@@ -24,44 +24,47 @@ QrAPI.prototype = {
         return $.post(
             this.apiUrl,
             {
-                qrcode: barcode,
-                password: this.apiPassword
+                "qrcode": barcode,
+                "password": this.apiPassword
             },
-            function success(data) {
-                if (data.status < 1) {
-                    var error;
-
-                    if (data.error.length <= 0) {
-                        error = "Unknown error occured";
-                    } else {
-                        error = data.error;
-                    }
-                    alert(error);
-
-                } else if (data.paid <= 0){
-                    alert("Hasn't paid!");
-
-                } else {
-                    var user = data.user.user,
-                        firstName = user.first_name,
-                        lastName = user.last_name,
-                        successMessage = "";
-
-                    if (firstName.length > 0) {
-                        successMessage = firstName;
-                        if (lastName.length > 0) {
-                            successMessage += lastName;
-                        }
-                    } else {
-                        successMessage = "Sign-in successful";
-                    }
-
-                    alert(successMessage);
-                }
-            },
+            this.scanRequestSuccess,
             'json'
         )
-    }
+    },
+
+    scanRequestSuccess: function scanRequestSuccess(data) {
+        if (data.status < 1) {
+            var error;
+
+            if (data.error.length <= 0) {
+                error = "Unknown error occured";
+            } else {
+                error = data.error;
+            }
+            alert(error);
+
+        } else if (data.paid < 1){
+            alert("Hasn't paid!");
+
+        } else {
+            var user = data.user.user,
+                firstName = user.first_name,
+                lastName = user.last_name,
+                successMessage = "";
+
+            if (firstName.length > 0) {
+                successMessage = firstName;
+                if (lastName.length > 0) {
+                    successMessage += lastName;
+                }
+            } else {
+                successMessage = "Sign-in successful";
+            }
+
+            console.log(data);
+            alert(successMessage);
+        }
+    },
 }
 
 function getSources() {

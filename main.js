804f5e2f48c313d5c08edc96ef60f6d2e95f49e8
Dominic May <jack.thatch@gmail.com>
2015-04-12 22:32:56 +0800

    Split out api

--- main.js
+++ main.js
@@ -4,68 +4,6 @@ navigator.getUserMedia = navigator.getUserMedia ||
                          navigator.msGetUserMedia;
 
 
-function QrAPI(apiUrl, apiPassword) {
-    this.apiUrl = apiUrl;
-    this.apiPassword = apiPassword;
-}
-QrAPI.prototype = {
-    getHistory: function getHistory() {
-        return $.getJSON(
-            this.apiUrl,
-            {
-                qrcode: "",
-                password: this.apiPassword
-            }
-        );
-    },
-
-    scanRequest: function scanRequest(barcode) {
-        'use strict';
-        return $.post(
-            this.apiUrl,
-            {
-                "qrcode": barcode,
-                "password": this.apiPassword
-            },
-            this.scanRequestSuccess,
-            'json'
-        )
-    },
-
-    scanRequestSuccess: function scanRequestSuccess(data) {
-        if (data.status < 1) {
-            var error;
-
-            if (data.error.length <= 0) {
-                error = "Unknown error occured";
-            } else {
-                error = data.error;
-            }
-            alert(error);
-
-        } else if (data.paid < 1){
-            alert("Hasn't paid!");
-
-        } else {
-            var user = data.user.user,
-                firstName = user.first_name,
-                lastName = user.last_name,
-                successMessage = "";
-
-            if (firstName.length > 0) {
-                successMessage = firstName;
-                if (lastName.length > 0) {
-                    successMessage += lastName;
-                }
-            } else {
-                successMessage = "Sign-in successful";
-            }
-
-            console.log(data);
-            alert(successMessage);
-        }
-    },
-}
 
 function getSources() {
     var def = $.Deferred();

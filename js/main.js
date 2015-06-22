1254d48b0c330143ea1975f94d72e530dda512b9
Dominic May <jack.thatch@gmail.com>
2015-06-22 12:07:38 +0800

    Enforce spacing

--- js/main.js
+++ js/main.js
@@ -221,7 +221,7 @@ var LogBox = React.createClass({
                     {this.props.log_messages.map(function(log, idx) {
                         return (
                             <div key={idx}>
-                                <p>{get_timestamp(log.timestamp)} {log.message}</p>
+                                <p>{get_timestamp(log.timestamp)}&nbsp;{log.message}</p>
                             </div>
                         );
                     })}

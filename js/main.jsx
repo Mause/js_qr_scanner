05afbc431dee08ab5104f795cf59e5cbe844da26
Dominic May <jack.thatch@gmail.com>
2015-06-28 16:42:39 +0800

    Don't be greedy

--- js/main.jsx
+++ js/main.jsx
@@ -121,7 +121,7 @@ var QRScanner = React.createClass({
             this.do_decode();
         }
 
-        setTimeout(this.timerCallback, 125 / 4);
+        setTimeout(this.timerCallback, 125);
     },
 
     do_decode: function do_decode() {
